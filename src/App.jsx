1a8436d76afa54d13bd973bacfafc410a92ebc40
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-05-18 13:17:08 +0300

    refactor: rename atributs for react sintaxis

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ import './scss/app.scss';
 function App() {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
               <PizzaBlock title="Чизбургер-пицца" price="149" />
               <PizzaBlock title="Чизбургер-пицца" price="149" />
               <PizzaBlock title="Чизбургер-пицца" price="149" />
