1e3a0427153695599672da354d2ec530079e8636
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-05-18 16:11:34 +0300

    feat: react categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,19 @@
+import React from 'react';
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Закрытые'];
+
   return (
     <>
       <div className="categories">
         <ul>
-          <li className="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
+          {categories.map((category, i) => (
+            <li onClick={() => setActiveIndex(i)} className={activeIndex === i ? 'active' : ''}>
+              {category}
+            </li>
+          ))}
         </ul>
       </div>
     </>
