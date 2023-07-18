61af8fe3e0adefc006347ec3c89a1afb1ce6c541
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-07-18 12:09:49 +0300

    feat: countinue inoput focus after input clear

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { setSearchValue } from '../../redux/slices/searchSlice';
@@ -8,10 +8,18 @@ function Search() {
   const dispatch = useDispatch();
   const searchValue = useSelector((state) => state.search.value);
 
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    dispatch(setSearchValue(''));
+    inputRef.current.focus();
+  };
+
   return (
     <div className="search-input">
       <img className={styles.icon} src="/search-icon.svg" alt="#" />
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(e) => dispatch(setSearchValue(e.target.value))}
         className={styles.input}
@@ -19,12 +27,7 @@ function Search() {
         type="text"
       />
       {searchValue && (
-        <img
-          onClick={() => dispatch(setSearchValue(''))}
-          className={styles.clear}
-          src="/icons/x-icon.svg"
-          alt="#"
-        />
+        <img onClick={onClickClear} className={styles.clear} src="/icons/x-icon.svg" alt="#" />
       )}
     </div>
   );
